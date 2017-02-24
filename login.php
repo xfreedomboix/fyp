7513167658baf4c52c963df98bb59628f4bc795c
boon <freedomboiong@hotmail.com>
2017-02-25 00:21:26 +0800

    add app

--- login.php
+++ login.php
@@ -8,14 +8,7 @@ header('Location: /index.php');
 }
 
 
-else{
-    echo '<script language="javascript">';
-  echo 'alert("Invalid ID or Password. Please try again.");';
-   echo 'window.location= "mainlogin.html";';
 
-  echo '</script>';
 
-  }
 
-}
 ?>
